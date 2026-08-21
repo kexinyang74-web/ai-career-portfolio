be3d98496d17302b53131855751619e1bed8b156
kexinyang74-web <315625802+kexinyang74-web@users.noreply.github.com>
2026-08-21 08:08:39 +0800

    vault backup: 2026-08-21 08:08:39

--- projects/python-practice/day9/05_word_count.py
+++ projects/python-practice/day9/05_word_count.py
@@ -1,2 +1,9 @@
 def count_words(text):
-    
+    return len(text.split())
+
+print(count_words("I love python"))
+
+def format_name(first,last):
+    return first.strip().title() + " " + last.strip().title()
+
+print(format_name(" ada ", " lovelace "))
