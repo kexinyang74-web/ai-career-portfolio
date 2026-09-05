1212e58f5122e065e8b8c8fe634b4c9e6148399d
kexinyang74-web <315625802+kexinyang74-web@users.noreply.github.com>
2026-09-05 19:42:59 +0800

    vault backup: 2026-09-05 19:42:59

--- projects/python-practice/day41/leetcode_409.py
+++ projects/python-practice/day41/leetcode_409.py
@@ -11,7 +11,18 @@
 
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        raise NotImplementedError
+        from collections import Counter # 计数器
+        counter = Counter(s) # 统计s中每个字符出现的次数
+        result = 0 # 初始化结果为0
+        has_odd = False # 初始化是否有奇数为False
+        for char in counter: # 遍历s中每个字符
+            if counter[char] % 2 == 0: # 如果字符出现的次数为偶数
+                result += counter[char] # 将字符出现的次数加入结果
+            else: # 如果字符出现的次数为奇数
+                result += counter[char] - 1 # 将字符出现的次数减1加入结果
+                if not has_odd: # 如果还没有奇数
+                    has_odd = True # 设置为True
+        return result + (1 if has_odd else 0) # 返回结果
 
 
 if __name__ == "__main__":
